d3550ff0c60bf8ebb5eabe48c1b780419fb6ca4b
“Aisling <aisling1odonovan@gmail.comgit commit -m Initial>
2018-06-17 05:28:55 +0100

    Connect to MongoDB

--- World_Stats.py
+++ World_Stats.py
@@ -17,47 +17,43 @@ MONGODB_URI = os.getenv('MONGODB_URI', 'mongodb://heroku_7ssmmd91:oduotmmvrf4pvs
 DBS_NAME = os.getenv('MONGO_DB_NAME','heroku_7ssmmd91')
 COLLECTION_NAME = os.getenv('MONGO_COLLECTION_NAME','worldCountries')
 
+FIELDS = {
+    '_id': False,
+    'country': True,
+    'capital': True,
+    'population': True,
+    'languages': True,
+    'area': True,
+    'currency': True,
+    'continent': True,
+    'drive': True
+}
+
 @app.route("/")
 def index():
-    """
-    A Flask view to serve the main dashboard page.
-    """
     return render_template("index.html")
 
 
 @app.route("/worldStats/worldCountries")
 def stat_projects():
-    """
-    A Flask view to serve the project data from
-    MongoDB in JSON format.
-    """
 
-    # A constant that defines the record fields that we wish to retrieve.
-    FIELDS = {
-        '_id': False,
-        'country': True,
-        'capital': True,
-        'population': True,
-        'languages': True,
-        'area': True,
-        'currency': True,
-        'continent': True,
-        'drive': True
-    }
 
-    # Open a connection to MongoDB using a with statement such that the
-    # connection will be closed as soon as we exit the with statement
     # *** Run Locally ***#
     # with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
     # *** END: Run Locally ***#
-    with MongoClient(MONGO_URI) as conn:
-        # Define which collection we wish to access
-        collection = conn[DBS_NAME][COLLECTION_NAME]
-        # Retrieve a result set only with the fields defined in FIELDS
-        # and limit the the results to 55000
-        projects = collection.find(projection=FIELDS, limit=20000)
-        # Convert projects to a list in a JSON object and return the JSON data
-        return json.dumps(list(projects))
+
+    # * Congfigure to run on heroku *#
+    connection = MongoClient(MONGODB_URI)
+    # * END Congfigure to run on heroku *#
+
+    collection = connection[DBS_NAME][COLLECTION_NAME]
+    projects = collection.find(projection=FIELDS, limit=20000)
+    rjson_projects = []
+    for project in projects:
+        json_projects.append(project)
+    json_projects = json.dumps(json_projects)
+    connection.close()
+    return json_projects
 
 
 if __name__ == "__main__":
